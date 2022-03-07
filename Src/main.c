6cf02f033f2e49e63cdb4fd003dec550d567990f
CKD <chiangkuandi@gmail.com>
2022-03-08 03:19:48 +0800

    Push button to control LED (Not Yet)

--- Src/main.c
+++ Src/main.c
@@ -17,8 +17,10 @@ uint32_t *pPortOutReg = (uint32_t)0x48000414U;
 */
 // using define to make code clean
 #define RCC_AHBENR *(volatile uint32_t *)(0x40021014U)	// AHB peripheral clock enable register, 0x40021000(RCC base address) + 0x14(offset)
-#define GPIOB_MODER *(volatile uint32_t *)(0x48000400U) // GPIOB base address + 0x00(offset)
+#define GPIOB_MODER *(volatile uint32_t *)(0x48000400U) // GPIOB base address(0x48000400) + 0x00(offset)
+#define GPIOC_MODER *(volatile uint32_t *)(0x48000800U) // GPIOC base address(0x48000000) + 0x00(offset)
 #define GPIOB_ODR *(volatile uint32_t *)(0x48000414U)	// GPIOB base address + 0x14(offset)
+#define GPIOC_IDR *(volatile uint32_t *)(0x48000810U)	// GPIOC base address + 0x10(offset)
 
 int main(void)
 {
@@ -37,34 +39,39 @@ int main(void)
 	 * 0x48000414
 	 *
 	 */
-
 	// select uint32_t because all peripheral register are of 32 bits.
 
-	// 1. enable the clock for GPIOB peripheral in the AHB2
+	// 1. enable the clock for GPIOB and GPIOA peripheral in the AHB2
 	RCC_AHBENR |= (1 << 18); // means	0x00040000U, set IOPB EN to 1(datasheet)
-
+	RCC_AHBENR |= (1 << 19); // Open IOPC(Set IOPC EN to 1)
 	// 2. configure the mode of the IO pin as output
+	// Configuring PA0 as input, because we are reading data from it.
 	// The LED is on pin7 -> confirm 14th -> 1, 15th -> 0
 	// a. clear the 14th and 15th bit positions (CLEAR)
-	GPIOB_MODER &= (3 << 14); // means 0xFFFF3FFFU, set 14th and 15th to 0 (datasheet)
-							  // 3 means 11 in binary
-							  // also can write ~(1 << 14) & ~(1 << 15)
+	GPIOB_MODER &= ~(3 << 14); // means 0xFFFF3FFFU, set 14th and 15th to 0 (datasheet)
+							   // 3 means 11 in binary
+							   // also can write ~(1 << 14) & ~(1 << 15)
 	// b make 14th bit position as 1 (SET)
 	GPIOB_MODER |= (1 << 14); // means 0x00004000U, set 14th to 1 (datasheet)
 
+	GPIOC_MODER &= ~(3 << 13); // set MODER0 to 00 (input mode)
+
 	while (1)
 	{
-		// 3. SET 7th bit of the output data register to make I/O pin-7 HIGH
-		GPIOB_ODR |= (1 << 7); // means 0x0080
-
-		for (int i = 0; i < 1000000; i++)
-			;
-
-		// Turn the Led off
-		GPIOB_ODR &= ~(1 << 7); // set the postion to 0
-
-		for (int i = 0; i < 1000000; i++)
-			;
+		uint8_t pinStatus = (uint8_t)GPIOC_IDR & (1 << 13);
+		printf("%x\n", pinStatus);
+		if (pinStatus)
+		{
+			// 3. SET 7th bit of the output data register to make I/O pin-7 HIGH
+			GPIOB_ODR |= (1 << 7); // means 0x0080
+								   // printf("LED ON\n");
+		}
+		else
+		{
+			// Turn the Led off
+			GPIOB_ODR &= ~(1 << 7); // set the postion to 0
+									// printf("LED OFF\n");
+		}
 	}
 	return 0;
 }
