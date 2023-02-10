2b956fa72c52c84ea5102f568074286a52b120cc
Risheng1128 <hi4u29ck02@gmail.com>
2023-02-10 15:29:11 +0800

    Introduce ClangFormat to improve the coding style

--- Src/main.c
+++ Src/main.c
@@ -1,23 +1,21 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  Ri-Sheng Chen
-  * @brief   This file is a example
-  ******************************************************************************
-  */
+ * @file    main.c
+ * @author  Ri-Sheng Chen
+ * @brief   This file is a example
+ */
 
-#include "myusart.h"
 #include <stdio.h>
+#include "myusart.h"
 
-int main(void) 
+int main(void)
 {
-	MYUSART_Init();
-	int data;
-	while(1) {
-		printf("input a data!!\n");
-		scanf("%d", &data);
-		printf("data = %d\n", data);
-		fflush(stdin);	/* Claer buffer */
-	}
-	return 0;
+    MYUSART_Init();
+    int data;
+    while (1) {
+        printf("input a data!!\n");
+        scanf("%d", &data);
+        printf("data = %d\n", data);
+        fflush(stdin); /* Claer buffer */
+    }
+    return 0;
 }
