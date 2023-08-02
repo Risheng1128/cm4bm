4df9f89fd5a96c6532475bdfbf5546c5645b2b8c
Risheng1128 <hi4u29ck02@gmail.com>
2023-08-02 23:48:13 +0800

    Keep USART macros private
    To ensure that the USART register address is only used in `usart.c`,
    we should move these macros to `usart.c` to prevent them from being
    exposed to other files.

--- src/usart.c
+++ src/usart.c
@@ -26,6 +26,31 @@
 
 #include "usart.h"
 
+#define DEFAULT_F_CLK 8000000U
+#define BAUDRATE_38400 38400U
+
+/* AHB peripheral clock enable register */
+#define RCC_AHBENR *(volatile uint32_t *) 0x40021014U
+/* APB1 peripheral clock enable register */
+#define RCC_APB1ENR *(volatile uint32_t *) 0x4002101CU
+
+/* GPIO port mode register */
+#define GPIOD_MODE *(volatile uint32_t *) 0x48000C00U
+/* GPIO alternate function high register */
+#define GPIOD_AFRH *(volatile uint32_t *) 0x48000C24U
+/* control register 1 */
+#define USART3_CR1 *(volatile uint32_t *) 0x40004800U
+/* baudrate register */
+#define USART3_BRR *(volatile uint32_t *) 0x4000480CU
+/* Request register */
+#define USART3_RQR *(volatile uint32_t *) 0x40004818U
+/* Interrupt and status register */
+#define USART3_ISR *(volatile uint32_t *) 0x4000481CU
+/* Receive data register */
+#define USART3_RDR *(volatile uint32_t *) 0x40004824U
+/* Transmit data register */
+#define USART3_TDR *(volatile uint32_t *) 0x40004828U
+
 void USART_Init()
 {
     /**************************** GPIO Set ****************************/
