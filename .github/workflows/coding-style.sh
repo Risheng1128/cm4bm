5b885a4841a345676a85ad4f2b68168b71c8946f
Risheng1128 <hi4u29ck02@gmail.com>
2023-08-03 00:07:10 +0800

    Simplify the coding-style checking

--- .github/workflows/coding-style.sh
+++ .github/workflows/coding-style.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SOURCES=$(find $(git rev-parse --show-toplevel) | egrep "\.(c|cxx|cpp|h|hpp)\$")
+SOURCES=$(find $(git rev-parse --show-toplevel) | egrep "\.(c|h)\$")
 
 set -x
 
